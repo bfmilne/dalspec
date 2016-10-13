4529836706453e8101cee204d71d29b63b49eb48
bfmilne <bfmilne@gmail.com>
2016-10-13 15:46:49 +0100

    Update dalspec.py

--- dalspec.py
+++ dalspec.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python
 
-'''Script to extract absorption spectra from Dalton TDDFT calculations.
-   Currently only '*LINEAR' response calculations with '*SINGLE RESIDUE'.
-   '''
+'''
+    Script to extract absorption spectra from Dalton TDDFT calculations.
+    NOTE: Currently only '*LINEAR' response calculations with '*SINGLE RESIDUE'
+    option selected.
+
+    Copyright (C) 2016  Bruce F. Milne
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+'''
+
+__author__ = "Bruce F. Milne"
+__copyright__ = "Copyright (C) 2016 Bruce F. Milne"
+__license__ = "GPL"
+
 
 import numpy as np
 import sys
